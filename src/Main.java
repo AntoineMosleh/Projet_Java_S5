44bbe4d40434c1001d04b955420e237ae45c331c
Yvan <yvanthai@gmail.com>
2021-10-31 16:35:30 +0100

    début main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,25 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World !");
+		menuPirate();
+	}
+	
+	public static void menuPirate() {
+        Scanner sc = new Scanner(System.in);
+		System.out.println("1 ajouter une relation");
+		System.out.println("2 ajouter des préférences");
+		System.out.println("3 Fin");
+        int choix = sc.nextInt();
+		switch(choix) {
+		case 1:
+
+			break;
+		case 2:
+            
+            break;
+        case 3:
+
+            break;
+        }
 	}
 }
