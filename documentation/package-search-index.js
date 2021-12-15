9b74405090455dd90f8e7ba91e3ed1e4c1c33baf
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2021-12-15 19:26:07 +0100

    ajout dossier documentation & maj todos

---
+++ documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"up.mi.yt_am_bz.partage_butin.pirates"},{"l":"up.mi.yt_am_bz.partage_butin.programme"}];updateSearchResults();
